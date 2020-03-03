ed03ffd3c4361128c4de295c5e742c73289096f4
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2020-03-03 10:18:08 +0100

    Mongoose models for User and Post

--- server.js
+++ server.js
@@ -3,15 +3,23 @@ const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' });
+const User = require('./models/User');
+const Post = require('./models/Post');
 
-// Connect to the database
+// Connect to the MongoDB database
 mongoose
     .connect(process.env.MONGO_URI, {useNewUrlParser: true})
     .then(() => console.log('DB connected'))
     .catch(err => console.err);
 
+
+// Start the Apollo Server
 const server = new ApolloServer({
-    typeDefs
+    typeDefs,
+    context: {
+        User,
+        Post
+    }
 });
 
 server.listen().then(({ url }) => {
