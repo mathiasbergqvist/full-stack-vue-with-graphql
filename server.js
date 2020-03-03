77ff37b5f59885401e79e26698929d6a44c79bf6
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2020-03-03 10:35:45 +0100

    GQL type definitions

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
+
+// Read type defs from file path
+const filePath = path.join(__dirname, 'typeDefs.gql');
+const typeDefs = fs.readFileSync(filePath, 'utf-8');
+
 require('dotenv').config({ path: 'variables.env' });
 const User = require('./models/User');
 const Post = require('./models/Post');
