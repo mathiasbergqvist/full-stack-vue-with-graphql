6b76d3e0ddefdf8f89b4e85e1f5e3595bdba8fea
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2020-03-03 10:00:47 +0100

    Setup connection to MongoDB database

--- schema.js
+++ schema.js
@@ -4,9 +4,6 @@ const typeDefs = gql`
     type Query {
         getTodos: [Todo]
     }
-    type Mutation {
-        addTodo(task: String, completed: Boolean): Todo
-    }
     type Todo {
         task: String
         completed: Boolean
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env' });
+
+// Connect to the database
+mongoose
+    .connect(process.env.MONGO_URI, {useNewUrlParser: true})
+    .then(() => console.log('DB connected'))
+    .catch(err => console.err);
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+    typeDefs
 });
 
 server.listen().then(({ url }) => {
