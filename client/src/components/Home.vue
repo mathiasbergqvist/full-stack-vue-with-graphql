50f3279dc4a81b51d7388b25d8fe2f47cedaa679
Mathias <mathiasbergqvist88@gmail.com>
2020-03-09 12:51:45 +0100

    Add get posts query to Home vue component

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,5 +1,33 @@
 <template>
   <v-container>
     <h1>Home</h1>
+    <ul v-for="post in getPosts" :key="post._id">
+      <li>
+        {{post.title}}
+        {{post.imageUrl}}
+        {{post.description}}
+      </li>
+    </ul>
   </v-container>
 </template>
+
+<script>
+import gql from "graphql-tag";
+
+export default {
+  name: "Home",
+  apollo: {
+    getPosts: gql`
+      query {
+        getPosts {
+          _id
+          title
+          imageUrl
+          description
+          likes
+        }
+      }
+    `
+  }
+};
+</script>
