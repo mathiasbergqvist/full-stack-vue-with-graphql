494a004760567029d07eb06848216a5a4bd217e1
Mathias <mathiasbergqvist88@gmail.com>
2020-03-10 10:10:56 +0100

    Use ApolloQuery component to query for data in Posts component

--- client/src/components/Posts/Posts.vue
+++ client/src/components/Posts/Posts.vue
@@ -1,11 +1,40 @@
 <template>
   <v-container>
     <h1>Posts</h1>
+    <ApolloQuery :query="getPostsQuery">
+      <template slot-scope="{ result: {loading, error, data} }">
+        <div v-if="loading">Loading...</div>
+        <div v-else-if="error">Error!</div>
+        <ul v-else v-for="post in data.getPosts" :key="post._id">
+          <li>
+            {{post.title}}
+            {{post.imageUrl}}
+            {{post.description}}
+          </li>
+        </ul>
+      </template>
+    </ApolloQuery>
   </v-container>
 </template>
 
 <script>
+import gql from "graphql-tag";
 export default {
-  name: "Posts"
+  name: "Posts",
+  data() {
+    return {
+      getPostsQuery: gql`
+        query {
+          getPosts {
+            _id
+            title
+            imageUrl
+            description
+            likes
+          }
+        }
+      `
+    };
+  }
 };
 </script>
