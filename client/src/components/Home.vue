859195d342d226ae3dd196d7979c1a540cc260d6
Mathias <mathiasbergqvist88@gmail.com>
2020-03-10 15:17:49 +0100

    Show loading indicator on Home component

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,24 +1,36 @@
 <template>
-  <div>Home</div>
-  <!-- <v-container text-xs-center v-if="getPosts">
+  <v-container text-xs-center>
+    <v-layout row>
+      <v-dialog v-model="loading" persistent fullscreen>
+        <v-container fill-height>
+          <v-layout row justify-center align-center>
+            <v-progress-circular indeterminate :size="70" :width="7" color="secondary"></v-progress-circular>
+          </v-layout>
+        </v-container>
+      </v-dialog>
+    </v-layout>
     <v-flex xs12>
-      <v-carousel v-bind="{ 'cycle': true }" interval="3000">
-        <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl">
+      <v-carousel v-if="!loading && posts.length > 0" v-bind="{ 'cycle': true }" interval="3000">
+        <v-carousel-item v-for="post in posts" :key="post._id" :src="post.imageUrl">
           <h1 id="carousel__title">{{post.title}}</h1>
         </v-carousel-item>
       </v-carousel>
     </v-flex>
-  </v-container>-->
+  </v-container>
 </template>
 
 <script>
 import gql from "graphql-tag";
+import { mapGetters } from "vuex";
 
 export default {
   name: "Home",
   // Lifecycle hook for component created
   created() {
     this.handleGetCarouselPosts();
+  },
+  computed: {
+    ...mapGetters(["loaders", "posts"])
   },
   methods: {
     handleGetCarouselPosts() {
