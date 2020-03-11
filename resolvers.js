89f0ed368f50f50e600d6d1154e8c62df5240c15
Mathias <mathiasbergqvist88@gmail.com>
2020-03-11 11:55:23 +0100

    Hash user password on signup

--- resolvers.js
+++ resolvers.js
@@ -1,47 +1,49 @@
 module.exports = {
-    // Query
-    Query: {
-        getPosts: async (_, args, { Post }) => {
-            const posts = await Post.find({})
-                .sort({ createdDate: 'desc' })
-                .populate({
-                    path: 'createdBy',
-                    model: 'User'
-                });
-            return posts;
-        }
-    },
-    // Mutations
-    Mutation: {
-        // Object destruct args, context
-        signupUser: async (_, { username, email, password }, { User }) => {
-            const user = await User.findOne({ username });
-
-            if (user) {
-                throw new Error('User already exists');
-            }
-
-            const newUser = await new User({
-                username,
-                email,
-                password
-            }).save();
-
-            return newUser;
-        },
-        addPost: async (
-            _,
-            { title, imageUrl, categories, description, creatorId },
-            { Post }
-        ) => {
-            const newPost = await new Post({
-                title,
-                imageUrl,
-                categories,
-                description,
-                createdBy: creatorId
-            }).save();
-            return newPost;
-        }
+  // Query
+  Query: {
+    getPosts: async (_, args, { Post }) => {
+      const posts = await Post.find({})
+        .sort({ createdDate: "desc" })
+        .populate({
+          path: "createdBy",
+          model: "User"
+        });
+      return posts;
     }
+  },
+  // Mutations
+  Mutation: {
+    // Object destruct args, context
+    signupUser: async (_, { username, email, password }, { User }) => {
+      const user = await User.findOne({ username });
+
+      if (user) {
+        throw new Error("User already exists");
+      }
+
+      console.log("New user", username);
+
+      const newUser = await new User({
+        username,
+        email,
+        password
+      }).save();
+
+      return newUser;
+    },
+    addPost: async (
+      _,
+      { title, imageUrl, categories, description, creatorId },
+      { Post }
+    ) => {
+      const newPost = await new Post({
+        title,
+        imageUrl,
+        categories,
+        description,
+        createdBy: creatorId
+      }).save();
+      return newPost;
+    }
+  }
 };
