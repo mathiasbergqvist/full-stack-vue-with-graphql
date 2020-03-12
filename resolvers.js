c753a5e000799c678af5411b01f9b81cec1e84cb
Mathias <mathiasbergqvist88@gmail.com>
2020-03-12 10:04:00 +0100

    Create jwt token upon singin/signup

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,10 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const createToken = (user, secret, expiresIn) => {
+  const { username, email } = user;
+  return jwt.sign({ username, email }, secret, { expiresIn });
+};
 
 module.exports = {
   // Query
@@ -23,15 +29,13 @@ module.exports = {
         throw new Error("User already exists");
       }
 
-      console.log("New user", username);
-
       const newUser = await new User({
         username,
         email,
         password
       }).save();
 
-      return newUser;
+      return { token: createToken(newUser, process.env.SECRET, "1hr") };
     },
     singninUser: async (_, { username, password }, { User }) => {
       const user = await User.findOne({ username });
@@ -43,7 +47,7 @@ module.exports = {
       if (!isValidPassword) {
         throw new Error("Invalid Password!");
       }
-      return user;
+      return { token: createToken(user, process.env.SECRET, "1hr") };
     },
     addPost: async (
       _,
--- server.js
+++ server.js
@@ -1,36 +1,35 @@
-const { ApolloServer } = require('apollo-server');
-const mongoose = require('mongoose');
-const fs = require('fs');
-const path = require('path');
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
 
 // Import typeDefs and resolvers
-const filePath = path.join(__dirname, 'typeDefs.gql');
-const typeDefs = fs.readFileSync(filePath, 'utf-8');
-const resolvers = require('./resolvers');
+const filePath = path.join(__dirname, "typeDefs.gql");
+const typeDefs = fs.readFileSync(filePath, "utf-8");
+const resolvers = require("./resolvers");
 
 // Import env variables
-require('dotenv').config({ path: 'variables.env' });
+require("dotenv").config({ path: "variables.env" });
 
-const User = require('./models/User');
-const Post = require('./models/Post');
+const User = require("./models/User");
+const Post = require("./models/Post");
 
 // Connect to the MongoDB database
 mongoose
-    .connect(process.env.MONGO_URI, {useNewUrlParser: true})
-    .then(() => console.log('DB connected'))
-    .catch(err => console.err);
-
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log("DB connected"))
+  .catch(err => console.err);
 
 // Start the Apollo/GQL Server
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: {
-        User,
-        Post
-    }
+  typeDefs,
+  resolvers,
+  context: {
+    User,
+    Post
+  }
 });
 
 server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
+  console.log(`🚀 Server ready at ${url}`);
 });
