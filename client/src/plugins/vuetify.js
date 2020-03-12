f13eb54a3312b8049ebc571c5e6399be230b6fc6
Mathias <mathiasbergqvist88@gmail.com>
2020-03-12 20:29:43 +0100

    Add material icons to dependencies

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,21 +1,24 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    theme: {
-        themes: {
-            light: {
-                primary: '#bac7be',
-                secondary: '#c2e1c2',
-                accent: '#7dcd85',
-                anchor: '#778472',
-                error: '#f44336',
-                warning: '#ffeb3b',
-                info: '#2196f3',
-                success: '#8bc34a'
-            }
-        }
+  icons: {
+    iconfont: "mdiSvg"
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: "#bac7be",
+        secondary: "#c2e1c2",
+        accent: "#7dcd85",
+        anchor: "#778472",
+        error: "#f44336",
+        warning: "#ffeb3b",
+        info: "#2196f3",
+        success: "#8bc34a"
+      }
     }
+  }
 });
