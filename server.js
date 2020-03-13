9f24455d7bc2c4decd25e3ac2f3910286a6efbfc
Mathias <mathiasbergqvist88@gmail.com>
2020-03-13 15:21:11 +0100

    Verify user through jwt token in apollo server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer, AuthenticationError } = require("apollo-server");
 const mongoose = require("mongoose");
 const fs = require("fs");
 const path = require("path");
+const jwt = require("jsonwebtoken");
 
 // Import typeDefs and resolvers
 const filePath = path.join(__dirname, "typeDefs.gql");
@@ -20,16 +21,29 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(() => console.log("DB connected"))
+  .then(() => console.log("🔌 DB connected"))
   .catch(err => console.error(err));
+
+// Verify JWT Token passed from client
+const getUser = async token => {
+  if (token) {
+    try {
+      return await jwt.verify(token, process.env.SECRET);
+    } catch (err) {
+      throw new AuthenticationError(
+        "Your session has ended. Please sign in again."
+      );
+    }
+  }
+};
 
 // Start the Apollo/GQL Server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: {
-    User,
-    Post
+  context: async ({ req }) => {
+    const token = req.headers["authorization"];
+    return { User, Post, currentUser: await getUser(token) };
   }
 });
 
